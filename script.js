3c03ae5d32b10b743299da7f068e7b68166105bf
Abdul-Ui-Code <mmalikawan005@gmail.com>
2025-10-06 07:06:14 +0500

    Complete Website

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
-    // Toggle Mobile Menu Visibility
-    document.body.classList.toggle("show-mobile-menu");
+  document.body.classList.toggle("show-mobile-menu");
 });
 
 // Close menu when the close button is clicked
-menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+menuCloseButton.addEventListener("click", () => {
+  document.body.classList.remove("show-mobile-menu");
+});
+
+// Close menu when any nav link is clicked
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+  });
+});
+    
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, 
+    dynamicBullets: true, 
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+}); 
